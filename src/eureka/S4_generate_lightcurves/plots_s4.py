d362932c8c55db91a9938c7664640d113314a50c
Hannah Diamond-Lowe <h.diamondlowe@gmail.com>
2024-08-22 15:06:39 +0200

    updated S4 plots to have binning

--- src/eureka/S4_generate_lightcurves/plots_s4.py
+++ src/eureka/S4_generate_lightcurves/plots_s4.py
@@ -70,7 +70,14 @@ def binned_lightcurve(meta, log, lc, i, white=False):
                          transform=ax.transAxes, color=f'C{p}')
     else:
         plt.errorbar(lc.time.values-time_modifier, norm_lcdata, norm_lcerr,
-                     fmt='o', color=f'C{i}', mec=f'C{i}', alpha=0.2)
+                     fmt='.', color=f'C{i}', mec=f'C{i}', alpha=0.1)
+
+        nbins = int(len(lc.time.values)/100)
+        binned_time = util.binData_time(lc.time.values-time_modifier, lc.time.values-time_modifier, nbins)
+        binned_flux = util.binData_time(norm_lcdata, lc.time.values-time_modifier, nbins)
+        binned_unc = util.binData_time(norm_lcerr, lc.time.values-time_modifier, nbins, err=True)
+        plt.errorbar(binned_time, binned_flux, yerr=binned_unc, fmt='o', markersize=3, color=f'C{i}', markeredgecolor='k', ecolor='k', elinewidth=2, zorder=1000) # this is bad and hard-coded; will need to modify for spectral case
+
         mad = util.get_mad_1d(norm_lcdata)
         meta.mad_s4_binned.append(mad)
         log.writelog(f'    MAD = {np.round(mad).astype(int)} ppm')
